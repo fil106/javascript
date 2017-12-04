0e84f4be13bd7c784c55efc99e2f27e42f1655ad
fedorov25 <fil106@mail.ru>
2017-12-04 17:57:38 +0300

    Почти сделана игра, осталось реализовать подсчет быков...

--- hw03/game/common.js
+++ hw03/game/common.js
@@ -1,6 +1,9 @@
-var count = 1, userNumber, randNumber, minNum, maxNum;
+var randNumber = getNumber(4);
+var count = 1, userNumber, minNum, maxNum;
 
 minNum = 1000;
 maxNum = 9999;
 
 start();
+
+// console.log(getNumber(4));
--- hw03/game/functions.js
+++ hw03/game/functions.js
@@ -1,16 +1,16 @@
 function start() {
-    randNumber = getNumber(4);
-    console.log("randowNumber = " + randNumber);
+    console.log("randomNumber = " + randNumber);
 
-    hello();
-    game(getUserNumber());
+    // hello();
+    // game(getUserNumber());
+    console.log(getCow(randNumber, 1234));
 }
 
 function hello() {
     alert("Попробуем отгадать число?\nЯ задагал случайное число от " + minNum + " до " + maxNum + ", состоящее из неповторяющихся цифр! В качестве подсказок выступают 'коровы' (цифра угадана, но её позиция - нет) и 'быки' (когда совпадает и цифра и её позиция). Т.е. если загадано число 1234, а вы называете 6531, то результатом будет 1 корова (цифра 1) и 1 бык (цифра 3)");
 }
 
-function getRandom(min,max) {
+function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
@@ -18,47 +18,27 @@ function getNumber(len) {
     var resultNumbers = [];
     var currNumber, resultStr = '';
 
-    for (var i = 0; i < len; i++) {
-        if (i === 0) {
-            currNumber = getRandom(1, 9);
-            resultNumbers.push(currNumber);
-        } else if (i === 1) {
-            currNumber = getRandom(0, 9);
-            var check = true;
+    if(isNumeric(len) && len > 0) {
+        for (var i = 0; i < len; i++) {
+            if (i === 0) {
+                currNumber = getRandom(1, 9);
+                resultNumbers.push(currNumber);
+            } else {
+                currNumber = getRandom(0, 9);
+                var check = true;
 
-            while (check) {
-                if (resultNumbers[0] === currNumber) {
-                    currNumber = getRandom(0, 9);
-                } else {
-                    resultNumbers.push(currNumber);
-                    check = false;
-                }
-            }
-        } else if (i === 2) {
-            currNumber = getRandom(0, 9);
-            var check = true;
-
-            while (check) {
-                if (resultNumbers[0] === currNumber || resultNumbers[1] === currNumber) {
-                    currNumber = getRandom(0, 9);
-                } else {
-                    resultNumbers.push(currNumber);
-                    check = false;
-                }
-            }
-        } else if (i === 3) {
-            currNumber = getRandom(0, 9);
-            var check = true;
-
-            while (check) {
-                if (resultNumbers[0] === currNumber || resultNumbers[1] === currNumber || resultNumbers[2] === currNumber) {
-                    currNumber = getRandom(0, 9);
-                } else {
-                    resultNumbers.push(currNumber);
-                    check = false;
+                while(check) {
+                    if(resultNumbers.indexOf(currNumber) === -1) {
+                        resultNumbers.push(currNumber);
+                        check = false;
+                    } else {
+                        currNumber = getRandom(0, 9);
+                    }
                 }
             }
         }
+    } else {
+        return "Ошибка!";
     }
 
     for(var j in resultNumbers) {
@@ -74,6 +54,24 @@ function getUserNumber() {
     return userNumber;
 }
 
+function getCow(randomNum, userNum) {
+    var rNum = randomNum + '';
+    var uNum = userNum + '';
+    var cow = 0;
+
+    for(var i in rNum) {
+        if(rNum.indexOf(uNum[i]) >= 0) {
+            cow++;
+        }
+    }
+
+    return cow + " Коров";
+}
+
+function getBull(randomNum, userNum) {
+    // в процессе
+}
+
 function game(num) {
     if(num === null) {
         randNumber = 0;
@@ -84,13 +82,11 @@ function game(num) {
             if (num >= minNum && num <= maxNum) {
                 if(num < randNumber) {
                     count++;
-                    alert("Хорошая попытка, ваше число МЕНЬШЕ загаданного числа\nПопробуйте еще раз");
-                    console.log(randNumber + " < " + num);
+                    alert("Хорошая попытка, у Вас " + getCow(randNumber, userNumber) + " и " + getBull(randNumber, userNumber));
                     game(getUserNumber());
                 } else if(num > randNumber) {
                     count++;
-                    alert("Хорошая попытка, ваше число БОЛЬШЕ загаданного числа\nПопробуйте еще раз");
-                    console.log(randNumber + " < " + num);
+                    alert("Хорошая попытка, у Вас " + getCow(randNumber, userNumber) + " и " + getBull(randNumber, userNumber));
                     game(getUserNumber());
                 } else if(num == randNumber) {
                     console.log(randNumber + " < " + num);
